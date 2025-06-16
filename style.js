3b48f0c405974da54b2168525b063a9b02df0f70
Metabel <131139821+Metabel@users.noreply.github.com>
2025-06-16 12:56:42 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+function myfunction(){
+    // alert("hello, this is an alert")
+    confirm("are you sure")
+ }
+ function changecolor(){
+     document.getElementById("mybtn").style.backgroundColor="yellow"  
+ }
+ function changepara(){
+     document.getElementById("mypara").style.color="blue"
+ }
